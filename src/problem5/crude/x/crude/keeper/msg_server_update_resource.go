2b46abeaa761520d58cb60b96911195ac61e9dc6
Farrel Dwireswara Salim <90848662+sugiyem@users.noreply.github.com>
2024-01-02 22:33:58 +0700

    Implement update-resource

--- src/problem5/crude/x/crude/keeper/msg_server_update_resource.go
+++ src/problem5/crude/x/crude/keeper/msg_server_update_resource.go
@@ -2,16 +2,36 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	"crude/x/crude/types"
+
+	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) UpdateResource(goCtx context.Context, msg *types.MsgUpdateResource) (*types.MsgUpdateResourceResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	var resource = types.Resource{
+		Metadata: msg.Metadata,
+		Value:    msg.Value,
+		Creator:  msg.Creator,
+		Id:       msg.Id,
+	}
+
+	val, found := k.GetResource(ctx, msg.Id)
+	if !found {
+		// there is no resource with given id
+		return nil, errorsmod.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("No resource with id %d", msg.Id))
+	}
+	if msg.Creator != val.Creator {
+		// unauthorized update
+		return nil, errorsmod.Wrap(sdkerrors.ErrUnauthorized, "Unauthorized update. Owner doesn't match")
+	}
+
+	k.SetResource(ctx, resource)
 
 	return &types.MsgUpdateResourceResponse{}, nil
 }
--- src/problem5/crude/x/crude/keeper/resource.go
+++ src/problem5/crude/x/crude/keeper/resource.go
@@ -16,9 +16,24 @@ func (k Keeper) AppendResource(ctx sdk.Context, resource types.Resource) uint64 
 	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
 	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.ResourceKey))
 	appendedValue := k.cdc.MustMarshal(&resource)
+
 	store.Set(GetResourceIDBytes(resource.Id), appendedValue)
 	k.SetResourceCount(ctx, count+1)
+
 	return count
+}
+
+func (k Keeper) GetResource(ctx sdk.Context, id uint64) (val types.Resource, found bool) {
+	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
+	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.ResourceKey))
+	b := store.Get(GetResourceIDBytes(id))
+
+	if b == nil {
+		return val, false
+	}
+	k.cdc.MustUnmarshal(b, &val)
+
+	return val, true
 }
 
 func (k Keeper) GetResourceCount(ctx sdk.Context) uint64 {
@@ -26,10 +41,20 @@ func (k Keeper) GetResourceCount(ctx sdk.Context) uint64 {
 	store := prefix.NewStore(storeAdapter, []byte{})
 	byteKey := types.KeyPrefix(types.ResourceCountKey)
 	bz := store.Get(byteKey)
+
 	if bz == nil {
 		return 0
 	}
+
 	return binary.BigEndian.Uint64(bz)
+}
+
+func (k Keeper) SetResource(ctx sdk.Context, post types.Resource) {
+	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
+	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.ResourceKey))
+	b := k.cdc.MustMarshal(&post)
+
+	store.Set(GetResourceIDBytes(post.Id), b)
 }
 
 func (k Keeper) SetResourceCount(ctx sdk.Context, count uint64) {
@@ -37,6 +62,7 @@ func (k Keeper) SetResourceCount(ctx sdk.Context, count uint64) {
 	store := prefix.NewStore(storeAdapter, []byte{})
 	byteKey := types.KeyPrefix(types.ResourceCountKey)
 	bz := make([]byte, 8)
+
 	binary.BigEndian.PutUint64(bz, count)
 	store.Set(byteKey, bz)
 }
