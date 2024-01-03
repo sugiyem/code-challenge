d1c16986096d0da2ddf7aed92ba31b81cfd4cc01
Farrel Dwireswara Salim <90848662+sugiyem@users.noreply.github.com>
2024-01-03 16:26:47 +0700

    Implement list-resource

--- src/problem5/crude/x/crude/keeper/query_list_resource.go
+++ src/problem5/crude/x/crude/keeper/query_list_resource.go
@@ -2,9 +2,13 @@ package keeper
 
 import (
 	"context"
+	"strings"
 
 	"crude/x/crude/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"cosmossdk.io/store/prefix"
+	"github.com/cosmos/cosmos-sdk/runtime"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -14,10 +18,39 @@ func (k Keeper) ListResource(goCtx context.Context, req *types.QueryListResource
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
-	ctx := sdk.UnwrapSDKContext(goCtx)
+	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(goCtx))
+	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.ResourceKey))
 
-	// TODO: Process the query
-	_ = ctx
+	var resources []types.Resource
+	var count uint64 = 0
 
-	return &types.QueryListResourceResponse{}, nil
+	pageRes, err := query.Paginate(store, req.Pagination, func(key []byte, value []byte) error {
+		var resource types.Resource
+		if err := k.cdc.Unmarshal(value, &resource); err != nil {
+			return err
+		}
+
+		var cond = true
+		if !strings.Contains(resource.Metadata, req.MetadataFilter) {
+			cond = false
+		}
+		if resource.Value < req.ValueLow || resource.Value > req.ValueHigh {
+			cond = false
+		}
+
+		if cond {
+			resources = append(resources, resource)
+			count++
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	pageRes.Total = count
+
+	return &types.QueryListResourceResponse{Resource: resources, Pagination: pageRes}, nil
 }
